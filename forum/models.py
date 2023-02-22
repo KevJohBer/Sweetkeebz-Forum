f43ec07adf38488a65c10c269cd7c951345ac392
KevJohBer <kevin.bergstrom0313@gmail.com>
2023-02-22 15:57:55 +0000

    add upvote/downvote view from inside the full post

--- forum/models.py
+++ forum/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 
 class Post(models.Model):
@@ -11,12 +12,21 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     upvote = models.ManyToManyField(User, related_name='upvote', blank=True)
     downvote = models.ManyToManyField(User, related_name='downvote', blank=True)
+    author_name = models.CharField(max_length=80, null=True)
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("full_post", kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
 
     #  def vote_result(self):
         #  return self.upvote.count()
@@ -30,6 +40,3 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ['-created_on']
-
-    def __str__(self):
-        return f"comment {self.comment} by {self.post}"
--- forum/views.py
+++ forum/views.py
@@ -21,6 +21,12 @@ class fullPost(View):
     def get(self, request, slug=None, *args, **kwargs):
         post = get_object_or_404(Post.objects, slug=slug)
         comments = post.comments.order_by('created_on')
+        upvoted = False
+        downvoted = False
+        if post.upvote.filter(id=self.request.user.id).exists():
+            upvoted = True
+        if post.downvote.filter(id=self.request.user.id).exists():
+            downvoted = True
 
         return render(
             request,
@@ -28,6 +34,8 @@ class fullPost(View):
             {
                 'post': post,
                 'comments': comments,
+                'upvoted': upvoted,
+                'downvoted': downvoted,
                 'add_comment': postComment()
             },
         )
@@ -46,6 +54,24 @@ class fullPost(View):
         else:
             add_comment = postComment()
 
+    def upvote(self, request, slug):
+
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.upvote.filter(id=self.request.user.id).exists():
+            post.upvote.remove(request.user)
+        else:
+            post.upvote.add(request.user)
+
+    def downvote(self, request, slug):
+
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.downvote.filter(id=self.request.user.id).exists():
+            post.downvote.remove(request.user)
+        else:
+            post.downvote.add(request.user)
+
         return render(
             request,
             "post.html",
@@ -61,6 +87,8 @@ class addPost(View):
 
     def post(self, request, *args, **kwargs):
         form = postForm(data=request.POST)
+        form.instance.author = request.user
+        form.instance.author_name = request.user.username
         if form.is_valid():
             form.save()
             return redirect('home')
@@ -94,24 +122,36 @@ class updatePost(View):
 class upvote(View):
 
     def post(self, request, slug):
-        post = get_object_or_404(Post, slug=slug)
 
-        if post.upvote.filter(id=request.user.id).exists():
+        post = get_object_or_404(Post, slug=slug)
+        upvtd = post.upvote.filter(id=self.request.user.id).exists()
+        downvtd = post.downvote.filter(id=self.request.user.id).exists()
+
+        if upvtd:
             post.upvote.remove(request.user)
+        elif downvtd:
+            post.downvote.remove(request.user)
+            post.upvote.add(request.user)
         else:
             post.upvote.add(request.user)
 
-        return HttpResponseRedirect(reverse('index.html', args=[slug]))
+        return HttpResponseRedirect(reverse('full_post', args=[slug]))
 
 
 class downvote(View):
 
     def post(self, request, slug):
-        post = get_object_or_404(Post, slug=slug)
 
-        if post.downvote.filter(id=request.user.id).exists():
+        post = get_object_or_404(Post, slug=slug)
+        upvtd = post.upvote.filter(id=self.request.user.id).exists()
+        downvtd = post.downvote.filter(id=self.request.user.id).exists()
+
+        if downvtd:
             post.downvote.remove(request.user)
+        elif upvtd:
+            post.upvote.remove(request.user)
+            post.downvote.add(request.user)
         else:
             post.downvote.add(request.user)
 
-        return HttpResponseRedirect(reverse('index.html', args=[slug]))
+        return HttpResponseRedirect(reverse('full_post', args=[slug]))
