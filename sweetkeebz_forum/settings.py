d7c0b91b8b2e209a877658a80533a1e0210f3d6c
KevJohBer <kevin.bergstrom0313@gmail.com>
2023-03-31 07:14:06 +0000

    try fix css not loading

--- sweetkeebz_forum/settings.py
+++ sweetkeebz_forum/settings.py
@@ -140,7 +140,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
-STATIC_ROOT = '.../static/'
+
+STATIC_ROOT = str(BASE_DIR / 'staticfiles/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
