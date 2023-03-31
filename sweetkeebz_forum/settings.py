6cab920a4415e3c35cf8f6fd48528e4ddc1651da
KevJohBer <kevin.bergstrom0313@gmail.com>
2023-03-31 08:02:59 +0000

    link staticfiles to cloudinary

--- sweetkeebz_forum/settings.py
+++ sweetkeebz_forum/settings.py
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'forum.apps.ForumConfig',
 ]
 
@@ -139,11 +141,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
-
-STATIC_ROOT = str(BASE_DIR / 'staticfiles/')
+STATICFILES_STORAGE = 'cloudinary_stoarge.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
